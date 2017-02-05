4bbb039a0544a7fd01a191f23d7d1aadf0e375d3
hotfi <hotfi@192.168.1.2>
2017-02-05 15:29:34 -0500

    made fluctation class

---
+++ GroupProject/src/stockMarket/Fluctuation.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package stockMarket;
+
+/**
+ * @author hotfi
+ *
+ */
+public class Fluctuation {
+
+	/**
+	 * FIELDS
+	 */
+	public Fluctuation() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
