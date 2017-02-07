ba95702c1a15fe4c781c0ce550b9364b9fd42d1a
Student 6 <Student 6@10.8.33.155>
2017-02-07 14:02:35 -0500

    user

---
+++ GroupProject/src/stockMarket/UserScreen.java
@@ -0,0 +1,14 @@
+package stockMarket;
+
+public class UserScreen {
+
+	public UserScreen() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
