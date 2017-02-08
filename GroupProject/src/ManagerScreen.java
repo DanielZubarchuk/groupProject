34d38b4d134e4c4677089e3cd909e5e741dfe79a
BigK <BigK@RayForComWin7090>
2017-02-08 00:40:53 -0500

    idk how to start ._.

---
+++ GroupProject/src/ManagerScreen.java
@@ -0,0 +1,33 @@
+import guiPractice8.component.ClickableGraphic;
+
+/**
+ * 
+ */
+
+/**
+ * @author Katherine
+ *
+ */
+public class ManagerScreen extends ClickableGraphic {
+
+	public ManagerScreen(int x, int y, String imageLocation) {
+		super(x, y, imageLocation);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ManagerScreen(int x, int y, double scale, String imageLocation) {
+		super(x, y, scale, imageLocation);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ManagerScreen(int x, int y, int w, int h, String imageLocation) {
+		super(x, y, w, h, imageLocation);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
