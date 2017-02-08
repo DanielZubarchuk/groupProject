08c9b7895ea6b440cb04f28a40109c4a43101cdf
Moo <Risa@192.168.2.6>
2017-02-08 02:09:00 -0500

    ff

--- GroupProject/src/stockMarket/UserScreen.java
+++ GroupProject/src/stockMarket/UserScreen.java
@@ -1,14 +1,22 @@
 package stockMarket;
 
-public class UserScreen {
+import java.util.List;
 
-	public UserScreen() {
+import guiPractice8.component.ClickableScreen;
+import guiPractice8.component.Visible;
+
+public class UserScreen extends ClickableScreen{
+
+
+	public UserScreen(int width, int height) {
+		super(width, height);
 		// TODO Auto-generated constructor stub
 	}
 
-	public static void main(String[] args) {
+	@Override
+	public void initAllObjects(List<Visible> viewObjects) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
