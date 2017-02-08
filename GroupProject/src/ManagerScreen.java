7a8784a4c30e10fcafe583013c02b8107730847a
student 8 <student 8@10.8.33.157>
2017-02-08 12:55:00 -0500

    idkkk how do I start ._.

---
+++ GroupProject/src/ManagerScreen.java
@@ -0,0 +1,32 @@
+import java.util.List;
+
+import guiPractice8.component.ClickableScreen;
+import guiPractice8.component.Visible;
+
+/**
+ * @author Katherine
+ *
+ */
+public class ManagerScreen extends ClickableScreen {
+
+	public ManagerScreen(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initAllObjects(List<Visible> arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void display(){
+		
+	}
+
+}
