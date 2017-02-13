a5dc52ff24bfd574e87ff52f9aad5bead2d514ac
BigK <BigK@RayForComWin7090>
2017-02-13 02:01:15 -0500

    Idk wut I'm doing anymore TT.TT

--- GroupProject/src/PropertyButtonInterface.java
+++ GroupProject/src/PropertyButtonInterface.java
@@ -1,8 +1,15 @@
-
 public interface PropertyButtonInterface {
-	void setPrice(int x);
-	void setPayout(int x);
-	int getPrice();
-	int getPayout();
+	
+	public boolean isClicked();
+	public void setQuantity(int x);
+	public void setPrice(double x);
+	public void setPayout(double x);
+	public void setLevel(int x);
+	public int getQuantity();
+	public double getPrice();
+	public double getPayout();
+	public int getLevel();
+	//public int setTime();
+	//public int getTime();
 	
 }
--- GroupProject/src/Statistics.java
+++ GroupProject/src/Statistics.java
@@ -1,23 +1,54 @@
-
 public class Statistics {
-	public static int balance;
-	public static int[] propertyQuantityArray;
 	
-	private static void main(String[] args){
+	public static int currentBalance;
+	public static int lifetimeEarnings;
+	//public static int[] propertyQuantityArray;
+	//public static int[] propertyTimeArray;
+	//public static int[] multiplier;
+	//public static int numResets;
+	
+	public static PropertyButton property;
+	//public PropertyButton[] properties;
+	
+	private static void main(String[] args){ 
+		
+		init(property);
+		while(property.isClicked()){
+			setNewPrice(property, );
+			setNewPayout(property, );
+		}
 		
 	}
 	
-	public Statistics() {
-		// TODO Auto-generated constructor stub
+	private static void init(PropertyButton property){
+		property.setQuantity(1);
+		property.setLevel(0);
 	}
 	
-	private void setNewPrice(PropertyButton property){
-		int purchasingAmount = 
+	private void setNewPrice(PropertyButton property, double x){
+		
+		currentBalance -= property.getPrice();
+		double prctIncrease = x;
+		double currentPrice = property.getPrice();
+		property.setPrice(currentPrice*prctIncrease);
+		
 	}
 	
-	
-	
-	
-	
+	private void setNewPayout(PropertyButton property, double x){
+		
+		double prctIncrease = x;
+		if(property.getLevel() == 0){
+			currentBalance += property.getPayout();
+			double currentPayout = property.getPayout();
+			property.setPayout(currentPayout*prctIncrease);
+		}
+		else if(property.getLevel() != 0 && property.getQuantity() % 25 == 0){
+			prctIncrease++;
+			currentBalance += property.getPayout();
+			double currentPayout = property.getPayout();
+			property.setPayout(currentPayout*prctIncrease);
+		}	
+		
+	}
 	
 }
