cba5377deb4e461b01462aff2afd66d392a517f6
jleung0080 <jleung0080@bths.edu>
2017-02-13 02:32:15 -0500

    Refactored joey operation class

---
+++ GroupProject/src/JoeyOperation.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class JoeyOperation {
+	public static ArrayList<AmountButtonInterface> amountButtons;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private boolean canBuy(int userMoney, int cost){
+		return userMoney>cost;
+		
+	}
+	
+	
+	
+	
+
+}
