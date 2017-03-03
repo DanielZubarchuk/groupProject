5bc7cc2d8a4a8752fbaab5ffdc1e5bffc3cc8ef2
User <User@Acer>
2017-03-03 01:25:43 -0500

    kms

--- GroupProject/src/stockMarket/StockMainMenu.java
+++ GroupProject/src/stockMarket/StockMainMenu.java
@@ -358,7 +358,7 @@ private class MenuScreen extends ClickableScreen{
 					if(selectedStock == null || shareNumber ==0){
 						display.setText("You must select a stock and shares.");
 					}
-					if(!inventoryScreen.getStocksInventory().contains(selectedStock)){
+					if(!User.stocksInventory.contains(selectedStock)){
 						display.setText("You dont have that stock or enough shares to sell.");
 					}
 					else{
