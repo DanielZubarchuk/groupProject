7f5a5081c08b1760eb175fe7811876bd6664be8a
Student 6 <Student 6@10.8.33.155>
2017-03-03 13:29:06 -0500

    user

--- GroupProject/src/stockMarket/StockComponent.java
+++ GroupProject/src/stockMarket/StockComponent.java
@@ -21,8 +21,8 @@ public class StockComponent extends Component {
 	private int viewAll;
 	private final int ALL_STOCKS = 0;
 	private final int ALL_TRANSACTIONS = 1;
-	private final int _STOCK = 3;
-	private final int _TRANSACTION = 4;
+	private final int _STOCK = 2;
+	private final int _TRANSACTION = 3;
 	
 	public StockComponent(int x, int y, int w, int h, StockInventory inventory, int viewAll) {
 		super(x, y, w, h);
@@ -37,14 +37,14 @@ public class StockComponent extends Component {
 		g = clear();
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g.setColor(Color.black);
-		
+		g.drawRect(0, 0, getWidth() - 1, getHeight() - 1);
 		int y = 0;
 		System.out.println("A");
 		
 		
 		if(User.history.isEmpty()){
 			
-			g.drawString("You have no previous transactions!", 100, 55);
+			g.drawString("You have no previous transactions!", 4, 15);
 			System.out.println("B");
 			return;
 		}else{
