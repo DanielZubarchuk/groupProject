90d465d630d3d93fa163650366de6908c2aa85b1
jleung0080 <jleung0080@bths.edu>
2017-03-02 20:37:21 -0500

    asdf

--- GroupProject/src/RichardDemoInterface.java
+++ GroupProject/src/RichardDemoInterface.java
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-
-/**
- * @author Katherine
- */
-
-public interface RichardDemoInterface {
-	//Richard I need the quantity of the property
-	int getQuantity();
-=======
-/*
- * @author Joey / Katherine
- */
 public interface RichardDemoInterface {
 	int getCurrentAmount();
->>>>>>> refs/remotes/origin/adventureTeam
 }
