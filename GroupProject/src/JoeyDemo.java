81c43f2b8c9cf3e404a6f0e87a4b2db8cd3a5baa
jleung0080 <jleung0080@bths.edu>
2017-03-24 00:34:50 -0400

    ?

--- GroupProject/src/JoeyDemo.java
+++ GroupProject/src/JoeyDemo.java
@@ -1,12 +1,9 @@
-package Demos;
+
 
 import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 
-import AmountButton;
-import ManagerButton;
-import PropertyButton;
 import guiPractice8.GUIApplication;
 import guiPractice8.Screen;
 import guiPractice8.component.Action;
